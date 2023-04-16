6014f0906d0842c23961ded28d9eb01149b74ee1
Mr White <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-04-16 17:16:46 +0300

    Create objects.hpp

---
+++ map/objects.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <cstdint>
+namespace stats {
+  constexpr ::std::ptrdiff_t Misc = 0x71DF64;
+  constexpr ::std::ptrdiff_t Mode = 0x731C08;
+  constexpr ::std::ptrdiff_t Model = 0x718CD0;
+  constexpr ::std::ptrdiff_t Modelname = 0x731BFC;
+  constexpr ::std::ptrdiff_t Models = 0x718CD8;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+}
