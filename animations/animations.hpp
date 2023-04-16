3a84c30bca740aa8101faedc81f9d56a122721f5
Mr White <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-04-16 17:23:10 +0300

    Create animations.hpp

---
+++ animations/animations.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstdint>
+namespace animations {  
+  constexpr ::std::ptrdiff_t Namedanimation_1 = 0x731BD4;
+  constexpr ::std::ptrdiff_t Namedanimation = 0x727D18;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+}
