1523b708686264196137be723f81097ad07dbf48
Mr White <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-04-16 17:14:52 +0300

    Create minigames.hpp

---
+++ map/minigames.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <cstdint>
+namespace minigames {
+  constexpr ::std::ptrdiff_t Minbash = 0x731CB8;
+  constexpr ::std::ptrdiff_t MiniGameLevel1 = 0x731C90;
+  constexpr ::std::ptrdiff_t Minigamelevel2 = 0x731C80;
+  constexpr ::std::ptrdiff_t Minigamelevel3 = 0x731C70;
+  constexpr ::std::ptrdiff_t Minigamelevel5 = 0x731C50;
+  constexpr ::std::ptrdiff_t Minigamelevel6 = 0x731C40;
+  constexpr ::std::ptrdiff_t Minigamelevel7 = 0x731C30;
+  constexpr ::std::ptrdiff_t Minigamelevel8 = 0x731C20;
+  constexpr ::std::ptrdiff_t Minigamelevel9 = 0x731C10;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+}
