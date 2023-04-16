097de0c47ba54716a5355172f49024bdcce90756
Mr White <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-04-16 17:18:14 +0300

    Create Music.hpp

---
+++ world/Music.hpp
@@ -0,0 +1,11 @@
+
+#pragma once
+#include <cstdint>
+namespace Music {  
+  constexpr ::std::ptrdiff_t Music = 0x731BF4;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+}
