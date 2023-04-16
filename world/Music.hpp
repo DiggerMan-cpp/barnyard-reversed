76851c3aa502d5d0a9c8426ce186b142b52cfb7e
Mr White <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-04-16 17:19:29 +0300

    Update Music.hpp

--- world/Music.hpp
+++ world/Music.hpp
@@ -3,8 +3,8 @@
 #include <cstdint>
 namespace Music {  
   constexpr ::std::ptrdiff_t Music = 0x731BF4;
-  constexpr ::std::ptrdiff_t  = 0x;
-  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t Nholonomicrate = 0x731BE4;
+  constexpr ::std::ptrdiff_t Name_0 = 0x71795C;
   constexpr ::std::ptrdiff_t  = 0x;
   constexpr ::std::ptrdiff_t  = 0x;
   constexpr ::std::ptrdiff_t  = 0x;
