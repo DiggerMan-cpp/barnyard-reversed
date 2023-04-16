baab7fcd22c767842f88ac98999c82997092d448
Mr White <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-04-16 03:30:09 +0300

    Create world_events.hpp

---
+++ world/world_events.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <cstdint>
+namespace stats {
+  constexpr ::std::ptrdiff_t  aTimeofday = 0x72A4CC;
+  constexpr ::std::ptrdiff_t  aPhaseofday = 0x72A4D8;
+  constexpr ::std::ptrdiff_t  dayofmonth = 0x72A4E4;
+  constexpr ::std::ptrdiff_t  AGameTimeRequiremen = 0x72A504;
+  constexpr ::std::ptrdiff_t  Doorzone = 0x718EF4;
+  constexpr ::std::ptrdiff_t  Sleepzone = 0x718F00;
+  constexpr ::std::ptrdiff_t  Savezone= 0x718F0C;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+}
