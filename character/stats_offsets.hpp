11614f5228e9efbaba91ab660438b9ba07f288ca
Mr White <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-04-16 03:24:56 +0300

    Create stats_offsets.hpp

---
+++ character/stats_offsets.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <cstdint>
+namespace stats {
+  constexpr ::std::ptrdiff_t Max_Health = 0x731CC8;
+  constexpr ::std::ptrdiff_t Max_Items = 0x731CEC;
+  constexpr ::std::ptrdiff_t milkmaxammo = 0x71E370;
+  constexpr ::std::ptrdiff_t milkfiredelay = 0x71E37C;
+  constexpr ::std::ptrdiff_t milkfireaccuracy = 0x71E38C;
+  constexpr ::std::ptrdiff_t money = 0x72C730;
+  constexpr ::std::ptrdiff_t unhappy = 0x;
+  constexpr ::std::ptrdiff_t love = 0x72C760;
+  constexpr ::std::ptrdiff_t hate = 0x72C758;
+  constexpr ::std::ptrdiff_t happy = 0x72C768;
+  constexpr ::std::ptrdiff_t food = 0x72C750;
+  constexpr ::std::ptrdiff_t friends = 0x72C748;
+  constexpr ::std::ptrdiff_t happy = 0x;
+}
