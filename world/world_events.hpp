173876656bf8edf39ffb5611745adc97c7760a3b
Mr White <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-04-16 17:22:01 +0300

    Update world_events.hpp

--- world/world_events.hpp
+++ world/world_events.hpp
@@ -9,10 +9,10 @@ namespace stats {
   constexpr ::std::ptrdiff_t  Sleepzone = 0x718F00;
   constexpr ::std::ptrdiff_t  Savezone= 0x718F0C;
   constexpr ::std::ptrdiff_t  Objectareause = 0x731BBC;
-  constexpr ::std::ptrdiff_t  Objectbreak = 0x;
-  constexpr ::std::ptrdiff_t  Objectequip = 0x;
-  constexpr ::std::ptrdiff_t  Objectthrow = 0x;
-  constexpr ::std::ptrdiff_t  Objectunequip= 0x;
+  constexpr ::std::ptrdiff_t  Objectbreak = 0x731BB0;
+  constexpr ::std::ptrdiff_t  Objectequip = 0x731BA4;
+  constexpr ::std::ptrdiff_t  Objectthrow = 0x731B98;
+  constexpr ::std::ptrdiff_t  Objectunequip= 0x731B88;
   constexpr ::std::ptrdiff_t  Objectuse  = 0x731B7C;
   constexpr ::std::ptrdiff_t  = 0x;
   constexpr ::std::ptrdiff_t  = 0x;
