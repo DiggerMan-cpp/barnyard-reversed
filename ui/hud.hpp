8b099ca859f883036370d3d09a962b079f832ab0
Mr White <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-04-16 03:42:17 +0300

    Create hud.hpp

---
+++ ui/hud.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <cstdint>
+namespace stats {
+  constexpr ::std::ptrdiff_t  clockwhite = 0x73A984;
+  constexpr ::std::ptrdiff_t  Clockred = 0x73A6D8;
+  constexpr ::std::ptrdiff_t  Stopwatch = 0x73A6E4;
+  constexpr ::std::ptrdiff_t  clockyellow = 0x73A6CC;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+  constexpr ::std::ptrdiff_t  = 0x;
+}
